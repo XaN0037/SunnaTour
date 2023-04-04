56a8f3e65d2414829f90afe4deac55ad28abda86
Khasan <info.kamalov@gmail.com>
2023-04-04 09:53:48 +0500

    Contract to exel

--- api/v1/payment/payme/views.py
+++ api/v1/payment/payme/views.py
@@ -54,12 +54,12 @@ class Payme(APIView):
             print(amount)
             if not order:
                 order = Order.objects.create(amount=int(amount), bron_id=bron_id)
-            amount *= 100
+            # amount /= 100
             print(amount)
             pay_link = GeneratePayLink(
                 order_id=order.id,
                 amount=Decimal(amount)
-            ).generate_link()
+            ).to_tiyin(amount).generate_link()
         except Exception as e:
             return Response(
                 {
